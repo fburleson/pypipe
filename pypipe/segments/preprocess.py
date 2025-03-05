11474074a8cb95d09273f869c2fb05c74487017f
Joel Burleson <joelburleson0104@gmail.com>
2025-03-05 22:25:18 +0100

    feat: preprocessing transformers

---
+++ pypipe/segments/preprocess.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
+from sklearn.compose import ColumnTransformer
+from pypipe.compose import Transformer
+
+
+class Subset(Transformer):
+    def __init__(self, columns: list[str], exclude: bool = False):
+        self.columns = columns
+        self.exclude = exclude
+
+    def transform(self, data: pd.DataFrame) -> pd.DataFrame:
+        if self.exclude:
+            return data.drop(self.columns, axis=1)
+        return data[self.columns]
+
+
+class Impute(Transformer):
+    def __init__(self, columns: list[str], exclude: bool = False):
+        self.columns = columns
+        self.exclude = exclude
+
+    def transform(self, data: pd.DataFrame) -> pd.DataFrame:
+        subset: pd.DataFrame = Subset(self.columns, self.exclude)(data)
+        mean_columns = subset.select_dtypes(include="number").columns
+        mode_columns = subset.select_dtypes(include="object").columns
+        transformer: ColumnTransformer = ColumnTransformer(
+            transformers=[
+                ("mean", SimpleImputer(strategy="mean"), mean_columns),
+                ("mode", SimpleImputer(strategy="most_frequent"), mode_columns),
+            ],
+            remainder="passthrough",
+            verbose_feature_names_out=False,
+        )
+        return pd.DataFrame(
+            transformer.fit_transform(data.replace([None], np.nan)),
+            columns=transformer.get_feature_names_out(),
+            index=data.index,
+        ).astype(data.dtypes)
+
+
+class Encode(Transformer):
+    def __init__(self, columns: list[str], exclude: bool = False):
+        self.columns = columns
+        self.exclude = exclude
+
+    def transform(self, data: pd.DataFrame) -> pd.DataFrame:
+        subset: pd.DataFrame = Subset(self.columns, self.exclude)(data)
+        multiclass = subset.loc[:, subset.nunique() > 2].columns
+        binary = subset.loc[:, subset.nunique() <= 2].columns
+        transformer: ColumnTransformer = ColumnTransformer(
+            transformers=[
+                ("onehot", OneHotEncoder(), multiclass),
+                ("label", OrdinalEncoder(), binary),
+            ],
+            remainder="passthrough",
+            verbose_feature_names_out=False,
+        )
+        return pd.DataFrame(
+            transformer.fit_transform(data),
+            columns=transformer.get_feature_names_out(),
+            index=data.index,
+        )
