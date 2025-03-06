f53fc4e533614801efa415a7ac9029bde47ec943
Joel Burleson <joelburleson0104@gmail.com>
2025-03-06 15:45:16 +0100

    feat: added multitransformers for branching pipes

--- pypipe/compose.py
+++ pypipe/compose.py
@@ -11,9 +11,27 @@ class Transformer(ABC):
         return self.transform(*args, **kwargs)
 
 
-class Pipeline(Transformer):
+class MultiTransformer(Transformer):
     def __init__(self, transformers: list[Transformer]):
         self.transformers = transformers
+
+    def transform(self, data) -> tuple[Any]:
+        if len(self.transformers) == 1:
+            return self.transformers[0](*data)
+        output: list = []
+        for i, data_i in enumerate(data):
+            output.append(self.transformers[i](data_i))
+        return tuple(output)
+
+
+class Pipeline(Transformer):
+    def __init__(self, pipeline: list[Transformer]):
+        self.transformers: list[Transformer] = []
+        for segment in pipeline:
+            if isinstance(segment, list):
+                self.transformers.append(MultiTransformer(segment))
+            else:
+                self.transformers.append(segment)
 
     def transform(self, *args, **kwargs) -> Any:
         out = self.transformers[0](*args, **kwargs)
