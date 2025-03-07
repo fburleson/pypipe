16bf89bdec3e4b5b05fbacfab9af767beacdec80
Joel Burleson <joelburleson0104@gmail.com>
2025-03-07 14:00:16 +0100

    feat: added runnable examples

---
+++ examples/iris.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
+from pypipe.compose import Pipeline
+from pypipe.models import LogisticClassifier
+from pypipe.segments.preprocess import TrainTestSplit, MinMaxScale
+from pypipe.segments.util import ToPandas, ToNumpy, Concat, Passthrough
+
+
+def main():
+    raw = load_iris()
+    model: LogisticClassifier = LogisticClassifier()
+    format_data: Pipeline = Pipeline(
+        [
+            [ToPandas(raw.feature_names), ToPandas(["species"])],
+            Concat(),
+        ]
+    )
+    preprocess: Pipeline = Pipeline(
+        [
+            TrainTestSplit(
+                [
+                    "sepal length (cm)",
+                    "sepal width (cm)",
+                    "petal length (cm)",
+                    "petal width (cm)",
+                ],
+                ["species"],
+            ),
+            [MinMaxScale(), MinMaxScale(), Passthrough(), Passthrough()],
+            [ToNumpy()] * 4,
+        ]
+    )
+    predict: Pipeline = Pipeline(
+        [
+            [model, Passthrough()],
+            [ToPandas()] * 2,
+            Concat(),
+        ]
+    )
+    data: pd.DataFrame = format_data((raw.data, raw.target))
+    print(data)
+    X_train, X_test, y_train, y_test = preprocess(data)
+    model.train(X_train, np.squeeze(y_train))
+    predictions: np.ndarray = predict((X_test, y_test)).to_numpy().T
+    print(f"{accuracy_score(predictions[0], predictions[1]):.2%}")
+
+
+if __name__ == "__main__":
+    main()
--- run.py
+++ run.py
@@ -1,2 +1,33 @@
+import subprocess
+
+
+def main():
+    models: dict = {
+        "iris": [
+            ["examples/iris.py"],
+            """A simple multi classifier using logistic regression and the classic iris data set""",
+        ],
+    }
+    is_exit = "n"
+    while is_exit != "y":
+        args: list[str] = input(
+            f'choose a script to run (type "help" to see options) {tuple(models.keys())}: '
+        ).split()
+        try:
+            if len(args) == 0:
+                continue
+            if "help" in args:
+                print("-d\tto see the description of a model")
+                continue
+            if "-d" in args:
+                print(models[args[0]][1])
+                continue
+            cmd: list[str] = ["python3", *models[args[0]][0]]
+            subprocess.run(cmd)
+        except KeyError:
+            print(f"{args[0]} is invalid input")
+        is_exit: str = input("exit? (y/n): ")
+
+
 if __name__ == "__main__":
-    print("Nothing here yet...")
+    main()
